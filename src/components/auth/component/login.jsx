10cc26e85169d31bd091e4773971b8c9e501ea11
MoehammadRizky <mamankirfan@gmail.com>
2024-02-17 13:47:42 +0700

    nyimpen token di Cookies

--- src/components/auth/component/login.jsx
+++ src/components/auth/component/login.jsx
@@ -1,28 +1,31 @@
 "use client";
 
 import React from "react";
+import Cookies from "js-cookie";
 
 export const Login = () => {
-async function handleSubmitLogin(event) {
-  event.preventDefault();
+  async function handleSubmitLogin(event) {
+    event.preventDefault();
 
-  const email = event.target.email.value;
-  const password = event.target.password.value;
+    const email = event.target.email.value;
+    const password = event.target.password.value;
 
-  const res = await fetch("https://eventmakers-api.fly.dev/auth/login", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      email,
-      password,
-    }),
-  });
+    const res = await fetch("https://eventmakers-api.fly.dev/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    });
 
-  const {payload, token} = await res.json();
-  localStorage.setItem("user", JSON.stringify(payload))
-}
+    const { payload, token } = await res.json();
+    localStorage.setItem("user", JSON.stringify(payload));
+
+    Cookies.set("token", token);
+  }
 
   return (
     <div>
@@ -34,4 +37,3 @@ async function handleSubmitLogin(event) {
     </div>
   );
 };
-
